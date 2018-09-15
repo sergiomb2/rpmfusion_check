0053328f4ee777e7847d61cf47e23ed93afce772
Sérgio M. Basto <sergio@serjux.com>
2018-09-15 15:48:03 +0100

    more developments (we want let query arched repos)

--- rpmfusion_check.src.phase1.sh
+++ rpmfusion_check.src.phase1.sh
@@ -2,28 +2,32 @@ versions=$(seq 8 27)
 versions=$(seq 26 28)
 branches=29
 rawhide=30
-#refresh="--refresh"
+refresh="--refresh"
+repo="-source"
+pac=src
+#repo=
+#pac=x86_64.rpm
 
 for version in $versions ; do
     echo repoquery $(printf %02d $version)
-    dnf repoquery $refresh --releasever=$version --arch=src --disablerepo='*' \
-        --enablerepo=rpmfusion-{,non}free{,-updates,-updates-testing}-source \
+    dnf repoquery $refresh --releasever=$version --disablerepo='*' \
+        --enablerepo=rpmfusion-{,non}free{,-updates,-updates-testing}$repo \
         --available --quiet --qf "%{name} %{repoid}" | \
-        sed 's|\(-[^-]\+\)\{2\}.src||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|' | \
+        sed "s|\(-[^-]\+\)\{2\}.$pac||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|" | \
         sort | uniq > rpmfusion_$(printf %02d $version).txt
 done
 # branched
 for version in $branches ; do
     echo repoquery branched $(printf %02d $version)
-    dnf repoquery $refresh --releasever=$version --arch=src --disablerepo='*' \
-        --enablerepo=rpmfusion-{,non}free{,-updates-testing}-source --available --quiet --qf "%{name} %{repoid}" | \
-        sed 's|\(-[^-]\+\)\{2\}.src||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|' | \
+    dnf repoquery $refresh --releasever=$version --disablerepo='*' \
+        --enablerepo=rpmfusion-{,non}free{,-updates-testing}$repo --available --quiet --qf "%{name} %{repoid}" | \
+        sed "s|\(-[^-]\+\)\{2\}.$pac||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|" | \
         sort | uniq > rpmfusion_$(printf %02d $version).txt
 done
 
 echo repoquery rawhide
-dnf repoquery $refresh --arch=src --disablerepo='*' --enablerepo=rpmfusion-{non,}free-rawhide-source \
+dnf repoquery $refresh --disablerepo='*' --enablerepo=rpmfusion-{non,}free-rawhide$repo \
 --available --quiet --qf "%{name} %{repoid}" | \
-sed 's|\(-[^-]\+\)\{2\}.src||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|' | sort |
-uniq > rpmfusion_$rawhide.txt
+sed "s|\(-[^-]\+\)\{2\}.$pac||; s| rpmfusion-free.*| rpmfusion-free|; s| rpmfusion-nonfree.*| rpmfusion-nonfree|" | \
+ sort | uniq > rpmfusion_$rawhide.txt
 
