157eb40c2c515b8a8dbccaeb242e4391beb2acf4
Sérgio M. Basto <sergio@serjux.com>
2022-02-11 23:05:30 +0000

    revisited check_mass_rebuild.sh

--- check_mass_rebuild.sh
+++ check_mass_rebuild.sh
@@ -1,9 +1,10 @@
-koji-rpmfusion latest-pkg --all f28-free > f28_free
-koji-rpmfusion latest-pkg --all f28-nonfree > f28_nonfree
-dnf repoquery --disablerepo='*' --enablerepo=rpmfusion-free-rawhide --srpm | sort | grep src$ | sed 's/\(-[^-]\+\)\{2\}$//' | uniq > f28_repofree.txt
-dnf repoquery --disablerepo='*' --enablerepo=rpmfusion-nonfree-rawhide --srpm | sort | grep src$ | sed 's/\(-[^-]\+\)\{2\}$//' | uniq > f28_repononfree.txt
-cat f28_free | sed 's/\(-[^-]\+\)\{2\} .*//' | sort > f28_free2
-cat f28_nonfree | sed 's/\(-[^-]\+\)\{2\} .*//' | sort > f28_nonfree2
-diff f28_free2 f28_repofree.txt -up | vir
-diff f28_nonfree2 f28_repononfree.txt -up | vir
+rawhide=36
+koji-rpmfusion latest-pkg --all f${rawhide}-free > f${rawhide}_free
+koji-rpmfusion latest-pkg --all f${rawhide}-nonfree > f${rawhide}_nonfree
+dnf repoquery --disablerepo='*' --enablerepo=rpmfusion-free-rawhide --srpm | sort | grep src$ | sed 's/\(-[^-]\+\)\{2\}$//' | uniq > f${rawhide}_repofree.txt
+dnf repoquery --disablerepo='*' --enablerepo=rpmfusion-nonfree-rawhide --srpm | sort | grep src$ | sed 's/\(-[^-]\+\)\{2\}$//' | uniq > f${rawhide}_repononfree.txt
+cat f${rawhide}_free | sed 's/\(-[^-]\+\)\{2\} .*//' | sort > f${rawhide}_free2
+cat f${rawhide}_nonfree | sed 's/\(-[^-]\+\)\{2\} .*//' | sort > f${rawhide}_nonfree2
+diff f${rawhide}_free2 f${rawhide}_repofree.txt -up | vi -R -
+diff f${rawhide}_nonfree2 f${rawhide}_repononfree.txt -up | vi -R -
 
